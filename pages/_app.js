ca97097377368f6b931f75cd6b263d939dc6148b
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-08-31 17:26:52 +0700

    init chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,51 @@
-import '../styles/globals.css'
+import React from "react";
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  bg: {
+    50: "#FFFFFF",
+    100: "#EFF4FF",
+    700: "#D7DDEC",
+    800: "#376AED",
+    900: "#E5E5E5",
+  },
+  btn: {
+    900: "#0D253C",
+    800: "#376AED",
+  },
+  text: {
+    50: "#FFFFFF",
+    100: "#7B8BB2",
+    200: "#CDCDCD",
+    300: "#0D253C",
+    400: "#376AED",
+  }
+}
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>News Today</title>
+
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
